929ad90c8927875899576ba3cf3736cde04463c2
Arun Kumar <arunkumarm1995@gmail.com>
2017-09-22 17:55:58 -0500

    Make cosmetic changes to findOrCreateSession function

--- index.js
+++ index.js
@@ -71,20 +71,23 @@ const fbMessage = (id, text) => {
 // sessionId -> {fbid: facebookUserId, context: sessionState}
 const sessions = {};
 
-const findOrCreateSession = (fbid) => {
+const findOrCreateSession = fbid => {
   let sessionId;
-  // Let's see if we already have a session for the user fbid
-  Object.keys(sessions).forEach(k => {
-    if (sessions[k].fbid === fbid) {
-      // Yep, got it!
-      sessionId = k;
-    }
+
+  Object.keys(sessions).forEach(id => {
+    if (sessions[id].fbid === fbid)
+      sessionId = id;
   });
+
   if (!sessionId) {
     // No session found for user fbid, let's create a new one
     sessionId = new Date().toISOString();
-    sessions[sessionId] = {fbid: fbid, context: {}};
+    sessions[sessionId] = {
+      fbid: fbid,
+      context: {}
+    };
   }
+
   return sessionId;
 };
 
