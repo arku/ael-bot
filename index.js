ecece6e5141aa7c9e0f893308e8f84f7c1466790
Arun Kumar <arunkumarm1995@gmail.com>
2017-09-23 17:18:09 -0500

    Make cosmetic changes

--- index.js
+++ index.js
@@ -93,7 +93,7 @@ const findOrCreateSession = fbid => {
 
 // Our bot actions
 const actions = {
-  send({sessionId}, {text}) {
+  send({ sessionId }, { text }) {
     // Our bot has something to say!
     // Let's retrieve the Facebook user whose session belongs to
     const recipientId = sessions[sessionId].fbid;
@@ -102,8 +102,8 @@ const actions = {
       // Let's forward our bot response to her.
       // We return a promise to let our bot know when we're done sending
       return fbMessage(recipientId, text)
-      .then(() => null)
-      .catch((err) => {
+      .then(_ => null)
+      .catch(err => {
         console.error(
           'Oops! An error occurred while forwarding the response to',
           recipientId,
@@ -112,9 +112,9 @@ const actions = {
         );
       });
     } else {
-      console.error('Oops! Couldn\'t find user for session:', sessionId);
+      console.error("Oops! Couldn't find user for session:", sessionId);
       // Giving the wheel back to our bot
-      return Promise.resolve()
+      return Promise.resolve();
     }
   },
   // You should implement your custom actions here
@@ -130,12 +130,14 @@ const wit = new Wit({
 
 // Starting our webserver and putting it all together
 const app = express();
+
 app.use(({method, url}, rsp, next) => {
   rsp.on('finish', () => {
     console.log(`${rsp.statusCode} ${method} ${url}`);
   });
   next();
 });
+
 app.use(bodyParser.json());
 
 // Webhook setup
@@ -155,62 +157,66 @@ app.post('/webhook', (req, res) => {
   // https://developers.facebook.com/docs/messenger-platform/webhook-reference
   const data = req.body;
 
-  if (data.object === 'page') {
-    data.entry.forEach(entry => {
-      entry.messaging.forEach(event => {
-        if (event.message && !event.message.is_echo) {
-          // Yay! We got a new message!
-          // We retrieve the Facebook user ID of the sender
-          const sender = event.sender.id;
-
-          // We retrieve the user's current session, or create one if it doesn't exist
-          // This is needed for our bot to figure out the conversation history
-          const sessionId = findOrCreateSession(sender);
-
-          // We retrieve the message content
-          const {text, attachments} = event.message;
-
-          if (attachments) {
-            // We received an attachment
-            // Let's reply with an automatic message
-            fbMessage(sender, 'Sorry I can only process text messages for now.')
-            .catch(console.error);
-          } else if (text) {
-            // We received a text message
-
-            // Let's forward the message to the Wit.ai Bot Engine
-            // This will run all actions until our bot has nothing left to do
-            wit.runActions(
-              sessionId, // the user's current session
-              text, // the user's message
-              sessions[sessionId].context // the user's current session state
-            ).then((context) => {
-              // Our bot did everything it has to do.
-              // Now it's waiting for further messages to proceed.
-              console.log('Waiting for next user messages');
-
-              // Based on the session state, you might want to reset the session.
-              // This depends heavily on the business logic of your bot.
-              // Example:
-              // if (context['done']) {
-              //   delete sessions[sessionId];
-              // }
-
-              // Updating the user's current session state
-              sessions[sessionId].context = context;
-            })
-            .catch((err) => {
-              console.error('Oops! Got an error from Wit: ', err.stack || err);
-            })
-          }
-        } else {
-          console.log('received event', JSON.stringify(event));
-        }
-      });
-    });
+  if (data.object !== 'page') {
+    return;
   }
+
+  data.entry.forEach(entry => {
+    entry.messaging.forEach(event => {
+      if (event.message && !event.message.is_echo) {
+        // Yay! We got a new message!
+        // We retrieve the Facebook user ID of the sender
+        const sender = event.sender.id;
+
+        // We retrieve the user's current session, or create one if it doesn't exist
+        // This is needed for our bot to figure out the conversation history
+        const sessionId = findOrCreateSession(sender);
+
+        // We retrieve the message content
+        const {text, attachments} = event.message;
+
+        if (attachments) {
+          // We received an attachment
+          // Let's reply with an automatic message
+          fbMessage(sender, 'Sorry I can only process text messages for now.')
+          .catch(console.error);
+        } else if (text) {
+          // We received a text message
+
+          // Let's forward the message to the Wit.ai Bot Engine
+          // This will run all actions until our bot has nothing left to do
+          wit.runActions(
+            sessionId, // the user's current session
+            text, // the user's message
+            sessions[sessionId].context // the user's current session state
+          ).then(context => {
+            // Our bot did everything it has to do.
+            // Now it's waiting for further messages to proceed.
+            console.log('Waiting for next user messages');
+
+            // Based on the session state, you might want to reset the session.
+            // This depends heavily on the business logic of your bot.
+            // Example:
+            // if (context['done']) {
+            //   delete sessions[sessionId];
+            // }
+
+            // Updating the user's current session state
+            sessions[sessionId].context = context;
+          })
+          .catch(err => {
+            console.error('Oops! Got an error from Wit: ', err.stack || err);
+          })
+        }
+      } else {
+        console.log('received event', JSON.stringify(event));
+      }
+    });
+  });
+
   res.sendStatus(200);
 });
 
 app.listen(PORT);
+
 console.log('Listening on :' + PORT + '...');
