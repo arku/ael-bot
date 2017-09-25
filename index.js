19e6cdbb4e335c378487d3ed6533217520425ecb
Arun Kumar <arunkumarm1995@gmail.com>
2017-09-25 11:28:48 -0500

    Setup primary intents and responses

--- index.js
+++ index.js
@@ -128,6 +128,55 @@ const wit = new Wit({
   logger: new log.Logger(log.INFO)
 });
 
+const queryWit = (text, n = 1) => {
+  return fetch(
+    `https://api.wit.ai/message?v=20170307&n=${n}&q=${encodeURIComponent(text)}`,
+    {
+      headers: {
+        Authorization: `Bearer ${WIT_TOKEN}`,
+        'Content-Type': 'application/json',
+      }
+    }
+  ).then(res => res.json());
+};
+
+// maps wit intents to responses
+const responses = {
+  get_location_in_airport: 'You can find our buses at the Airport parking lot, behind the Costa del Sol Hotel (Next to International fights arrival)',
+  get_schedule: 'You can find our timetables at this link: https://www.airportexpresslima.com/timetables/ \n Please be aware, for International flights it is recommended to be at the airport 3 hours before the flight and 2 hours before for domestic flights.',
+  get_info: 'You can find how our service works in this link: https://www.airportexpresslima.com/how-it-works/',
+  get_payment_info: 'You can pay our guides in cash on board the bus. Also you can buy your tickets at our airport counter or online at this link:  https://www.airportexpresslima.com/tickets/',
+  get_stops_info: 'We have 7 official stops in Miraflores. You can find more detailed information about our stops in this link: https://www.airportexpresslima.com/find-your-stop/',
+  get_prices: 'You can find our prices at this link: https://www.airportexpresslima.com/tickets/',
+  get_validity: 'Your ticket is valid for 6 months. You can use it on any bus, any day during those 6 months',
+  get_roundtrip_info: 'Our system only allows to select the departure date/time, as usually the passengers do not show up the day they indicate for their return, for that reason, the ticket is open for any return day/time. For your return, you only need to show the voucher that will be given on board on the day of your departure. Your ticket is valid for 6 months.'
+};
+
+const handleMessage = message => {
+  console.log(message);
+  return queryWit(message).then(({entities}) => {
+    console.log(entities);
+    const intent = firstEntity(entities, 'intent');
+    if (!intent) {
+      console.log('🤖  Try something else. I got no intent :)');
+      return;
+    }
+
+    if (intent.value in responses) {
+      return responses[intent.value];
+    }
+    return console.log(`🤖  ${intent.value}`);
+  });
+}
+
+const firstEntity = (entities, name)  => {
+  return entities &&
+    entities[name] &&
+    Array.isArray(entities[name]) &&
+    entities[name] &&
+    entities[name][0];
+}
+
 // Starting our webserver and putting it all together
 const app = express();
 
@@ -185,28 +234,32 @@ app.post('/webhook', (req, res) => {
 
           // Let's forward the message to the Wit.ai Bot Engine
           // This will run all actions until our bot has nothing left to do
-          wit.runActions(
-            sessionId, // the user's current session
-            text, // the user's message
-            sessions[sessionId].context // the user's current session state
-          ).then(context => {
-            // Our bot did everything it has to do.
-            // Now it's waiting for further messages to proceed.
-            console.log('Waiting for next user messages');
+          handleMessage(text)
+          .then(console.log)
+          .catch(console.err)
 
-            // Based on the session state, you might want to reset the session.
-            // This depends heavily on the business logic of your bot.
-            // Example:
-            // if (context['done']) {
-            //   delete sessions[sessionId];
-            // }
-
-            // Updating the user's current session state
-            sessions[sessionId].context = context;
-          })
-          .catch(err => {
-            console.error('Oops! Got an error from Wit: ', err.stack || err);
-          })
+          // wit.runActions(
+          //   sessionId, // the user's current session
+          //   text, // the user's message
+          //   sessions[sessionId].context // the user's current session state
+          // ).then(context => {
+          //   // Our bot did everything it has to do.
+          //   // Now it's waiting for further messages to proceed.
+          //   console.log('Waiting for next user messages');
+          //
+          //   // Based on the session state, you might want to reset the session.
+          //   // This depends heavily on the business logic of your bot.
+          //   // Example:
+          //   // if (context['done']) {
+          //   //   delete sessions[sessionId];
+          //   // }
+          //
+          //   // Updating the user's current session state
+          //   sessions[sessionId].context = context;
+          // })
+          // .catch(err => {
+          //   console.error('Oops! Got an error from Wit: ', err.stack || err);
+          // })
         }
       } else {
         console.log('received event', JSON.stringify(event));
